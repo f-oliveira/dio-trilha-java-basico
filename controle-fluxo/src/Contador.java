012fddd4a843fdd09ed855c00fd4f7a0588a4725
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2024-07-17 22:32:31 -0300

    feat: resolve flow control challenge.

---
+++ controle-fluxo/src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro parâmetro ");
+        int parametroUm = terminal.nextInt();
+        System.out.print("Digite o segundo parâmetro ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }
+        catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
+        }
+
+    }
+    private static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o numero " + i);
+        }
+    }
+}
